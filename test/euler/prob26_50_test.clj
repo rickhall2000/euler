1e508017d0845fb93be327294a9dfe376a952570
Richard Hall <rickhall2000@Richards-MacBook-Air.local>
2012-04-29 20:07:19 -0400

    Several test cases added

--- test/euler/prob26_50_test.clj
+++ test/euler/prob26_50_test.clj
@@ -5,3 +5,15 @@
 (deftest prob29-test
   (testing "there are 15 combinations a^b where a and b are > 2 and < 5"
     (is (= 15 (problem29 5)))))
+
+(deftest char-to-power-test
+  (testing "the char 3 raised to the 3rd power should yield 27"
+    (is (= 27 (char-to-power (first (seq "3")) 3)))))
+
+(deftest sum-digits-to-nth-test
+  (testing "the digits in 1634 to the 4th add up to 1634"
+    (is (= 1634 (sum-digits-to-nth 1634 4)))))
+
+(deftest prob30-test
+  (testing "sum of numbers can be written as 4th power of digits"
+    (is (= 19316 (problem30 4 9999)))))
